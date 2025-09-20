4d8e8e0e498a6fce4a560c9f205e58f1872fe237
Abhivanya <sbzabhishekyadav@gmail.com>
2025-09-20 05:38:45 -0400

    fixed validation middelware argument missed issue

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,8 +3,9 @@ import { requestDataValidator } from "../middelwares/requestValidator.js";
 const router = express.Router();
 
 import { getAllTasks, addTask } from "../controller/taskController.js";
+import { createTaskSchema } from "../validation/taskSchema.js";
 
 router.get("/tasks", getAllTasks);
-router.post("/tasks", requestDataValidator, addTask);
+router.post("/tasks", requestDataValidator(createTaskSchema), addTask);
 
 export default router;
